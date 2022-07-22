287a032a209729b5f994fe1a7fe7b10a094417bc
Wandwilson Almeida da silva <77290358+wandyalmeida@users.noreply.github.com>
2022-07-22 15:21:06 +0100

    Add type animation

---
+++ script.js
@@ -0,0 +1,15 @@
+// const fadeOut = ()  {
+//     const loaderWrapper =
+//     document.querySelector('.wrapper');
+//     loaderWrapper.classList.add('fade');
+// }
+
+
+// window.addEventListener('load', fadeOut);
+
+var typed = new Typed(".auto-type", {
+    strings: ["Developer Front-END", "Freelancer"],
+    typeSpeed: 75,
+    backSpeed: 75,
+    loop: true
+})

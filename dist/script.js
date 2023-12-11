bbdd481be3f121688b9cf4b2a9fcb9031e540f9b
Wandwilson Almeida Da Silva <77290358+wandyalmeida@users.noreply.github.com>
2023-12-11 12:32:31 +0000

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -44,8 +44,8 @@ function sendEmail(){
     console.log("Email sented")
     Email.send({
         SecureToken : "4eb02015-ff65-40b8-9efa-2d0212e511a9",
-        To : 'wandwilson.silva@gmail.com',
-        From : 'wandwilson.silva@gmail.com',
+        To : 'wandwilson.almeida@hotmail.com',
+        From : 'wandwilson.almeida@hotmail.com',
         Subject : "New oportunity",
         Body : "Name: " + document.getElementById("name").value
             + "<br> Email: " + document.getElementById("email").value
